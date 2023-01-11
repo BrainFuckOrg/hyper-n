bc085e1ff1e4bbbe4aed12283e6d3c8cac39cb6d
KosenkoAlexander <alexandkosenko@gmail.com>
2023-01-11 20:32:43 +0200

    .c and .h Sanya

---
+++ cmake-build-debug/hyperLibSanya.c
@@ -0,0 +1,6 @@
+//
+// Created by kosenko on 11.01.23.
+//
+
+#include "hyperLibSanya.h"
+
---
+++ cmake-build-debug/hyperLibSanya.h
@@ -0,0 +1,8 @@
+//
+// Created by kosenko on 11.01.23.
+//
+
+#ifndef HYPER_N_HYPERLIBSANYA_H
+#define HYPER_N_HYPERLIBSANYA_H
+
+#endif //HYPER_N_HYPERLIBSANYA_H

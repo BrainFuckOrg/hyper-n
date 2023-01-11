db9e7ba28376e94c2b9aafd548292f7f264c01c4
MaksymShkarupylo <shkarupilo2005@gmail.com>
2023-01-11 20:24:16 +0200

    add max lib

---
+++ hyperLibMax.c
@@ -0,0 +1,5 @@
+//
+// Created by maximus on 11.01.23.
+//
+
+#include "hyperLibMax.h"
---
+++ hyperLibMax.h
@@ -0,0 +1,8 @@
+//
+// Created by maximus on 11.01.23.
+//
+
+#ifndef HYPER_N_HYPERLIBMAX_H
+#define HYPER_N_HYPERLIBMAX_H
+
+#endif //HYPER_N_HYPERLIBMAX_H

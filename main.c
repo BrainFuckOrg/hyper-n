54f4b5df4c6e01ca9a9130519283001bda6a0463
KosenkoAlexander <alexandkosenko@gmail.com>
2023-01-11 21:08:47 +0200

    main change

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include "hyperLibSanya.h"
+#include "hyperLibMax.h"
 int main() {
     //printf("Hello, World!\n");
     printf("%d",hyper_n(2,3,5));
+    printf("%d", hyperN(2,3,5));
     return 0;
 }

a4d6db64a5138de3ec56df7041d8dcb8a4611858
KosenkoAlexander <alexandkosenko@gmail.com>
2023-01-12 15:38:12 +0200

    cyclic hyper_n

--- Python_version/hyper_n_Sanya.py
+++ Python_version/hyper_n_Sanya.py
@@ -6,3 +6,20 @@ def hyper_n(num1, num2, grade):
         for i in range(1, num2):
             result = hyper_n(num1, result, grade - 1)
         return result
+
+
+def hyper_n_norecursion(num1, num2, grade):
+    stack = [(num2, grade, 1)]
+    result=num1
+    count=0
+    while len(stack) > 0:
+        n2, g, i = stack.pop()
+        if g == 1:
+            result=num1+n2
+        else:
+            if i==1: result=num1
+            if i<n2:
+                stack.append((n2, g, i+1))
+                stack.append((result, g-1, 1))
+        #print(stack, result)
+    return result
